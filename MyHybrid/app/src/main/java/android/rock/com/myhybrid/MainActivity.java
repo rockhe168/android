48a3c76d1c990b0ed72b68a866ec4667c615d73a
何湘红 <xhhe@Ctrip.com>
2016-03-28 18:42:23 +0800

    myHybrid project

---
+++ MyHybrid/app/src/main/java/android/rock/com/myhybrid/MainActivity.java
@@ -0,0 +1,16 @@
+package android.rock.com.myhybrid;
+
+import android.os.Bundle;
+import android.rock.com.mylibrary.BaseActivity;
+
+/**
+ * Created by xhhe on 2016/3/28.
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
