ded68413c40a75b982be63499ec7afad40b34292
何湘红 <xhhe@Ctrip.com>
2016-05-20 19:39:06 +0800

    udpate

--- MyHybrid/app/src/main/java/android/rock/com/myhybrid/MainActivity.java
+++ MyHybrid/app/src/main/java/android/rock/com/myhybrid/MainActivity.java
@@ -2,15 +2,27 @@ package android.rock.com.myhybrid;
 
 import android.os.Bundle;
 import android.rock.com.mylibrary.BaseActivity;
+import android.rock.com.mylibrary.core.JsBridgeWebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 /**
  * Created by xhhe on 2016/3/28.
  */
 public class MainActivity extends BaseActivity {
 
+    private WebView mWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.myWebView);
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        mWebView.setWebChromeClient(new JsBridgeWebChromeClient());
+        mWebView.loadUrl("http://10.32.229.125:5389/hybird.html");
     }
 }
---
+++ MyHybrid/mylibrary/src/main/java/android/rock/com/mylibrary/core/JsBridgeWebChromeClient.java
@@ -0,0 +1,27 @@
+package android.rock.com.mylibrary.core;
+
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+/**
+ * Created by xhhe on 2016/5/20.
+ */
+public class JsBridgeWebChromeClient extends WebChromeClient{
+
+    @Override
+    public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+        return super.onJsPrompt(view, url, message, defaultValue, result);
+    }
+
+    @Override
+    public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+        return super.onJsConfirm(view, url, message, result);
+    }
+
+    @Override
+    public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+        return super.onJsAlert(view, url, message, result);
+    }
+}
